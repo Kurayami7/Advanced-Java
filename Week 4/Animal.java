f81fcb723bc3dab15549bdc4017c5cae69bdbdc1
Areaf <darkgulliotine7@gmail.com>
2023-06-12 19:23:36 -0400

    Week 4

---
+++ Week 4/Animal.java
@@ -0,0 +1,22 @@
+
+public class Animal{
+  
+private String name;
+private double topSpeed;
+
+  
+ public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getTopSpeed() {
+		return topSpeed;
+	}
+	public void setTopSpeed(double topSpeed) {
+		this.topSpeed = topSpeed;
+	}
+
+  
+}
